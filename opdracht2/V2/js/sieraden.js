107c4f6a38aefc95adc07b2517208c2c5f119790
Kim <kimgarrard030@gmail.com>
2019-05-18 10:22:29 +0200

    Versie 2 - css verbeterd + begin animaties carousel

--- opdracht2/V2/js/sieraden.js
+++ opdracht2/V2/js/sieraden.js
@@ -4,74 +4,110 @@
 
 /*eslint 'no-console': 0*/
 
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
+//
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+//
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+//
+// function showSlides(n) {
+//   var i;
+//   var slides = document.querySelectorAll(".mySlides");
+//   var dots = document.querySelectorAll(".dot");
+//   if (n > slides.length) {slideIndex = 1};
+//   if (n < 1) {slideIndex = slides.length};
+//
+//   for (slidesI of slides) {
+//       slidesI.classList.add("hide");
+//   }
+//   slides[slideIndex-1].classList.replace("hide","show");
+//
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   dots[slideIndex-1].className += " active";
+// };
+//
+//
+//
+//
+// window.onload = function() {
+//
+//     var dot1 = document.querySelector(".dot:nth-of-type(1)");
+//     var dot2 = document.querySelector(".dot:nth-of-type(2)");
+//     var dot3 = document.querySelector(".dot:nth-of-type(3)");
+//     var next = document.querySelector(".next");
+//     var prev = document.querySelector(".prev");
+//     var body = document.querySelector("body");
+//
+//     dot1.onclick = show1;
+//     dot2.onclick = show2;
+//     dot3.onclick = show3;
+//     next.onclick = showNext;
+//     prev.onclick = showPrev;
+//     body.onkeydown = getKeyAndMove;
+// };
+//
+// function show1() {
+//   currentSlide(1);
+// };
+//
+// function show2() {
+//   currentSlide(2);
+// };
+//
+// function show3() {
+//   currentSlide(3);
+// };
+//
+// function showNext() {
+//   plusSlides(1);
+// };
+//
+// function showPrev() {
+//   plusSlides(-1);
+// };
+//
+// function getKeyAndMove(e){
+// 		var key_code=e.which||e.keyCode;
+// 		switch(key_code){
+// 			case 37: //left arrow key
+// 				moveLeft();
+// 				break;
+// 			case 39: //right arrow key
+// 				moveRight();
+// 				break;
+// 		}
+// 	}
+//
+//   function moveLeft() {
+//     plusSlides(-1);
+//   };
+//
+//   function moveRight() {
+//     plusSlides(1);
+//   };
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  dots[slideIndex-1].className += " active";
-};
 
 
+var currentPicture = 1;
+var carouselDiv = document.querySelector("#carousel div");
 
+var nextButton = document.querySelector(".next");
+var prevButton = document.querySelector(".prev");
+var body = document.querySelector("body");
+var pic1 = document.querySelector("picture:nth-of-type(1)");
+var pic2 = document.querySelector("picture:nth-of-type(2)");
+var pic3 = document.querySelector("picture:nth-of-type(3)");
 
-window.onload = function() {
-
-    var dot1 = document.querySelector(".dot:nth-of-type(1)");
-    var dot2 = document.querySelector(".dot:nth-of-type(2)");
-    var dot3 = document.querySelector(".dot:nth-of-type(3)");
-    var next = document.querySelector(".next");
-    var prev = document.querySelector(".prev");
-    var body = document.querySelector("body");
-
-    dot1.onclick = show1;
-    dot2.onclick = show2;
-    dot3.onclick = show3;
-    next.onclick = showNext;
-    prev.onclick = showPrev;
-    body.onkeydown = getKeyAndMove;
-};
-
-function show1() {
-  currentSlide(1);
-};
-
-function show2() {
-  currentSlide(2);
-};
-
-function show3() {
-  currentSlide(3);
-};
-
-function showNext() {
-  plusSlides(1);
-};
-
-function showPrev() {
-  plusSlides(-1);
-};
+body.onkeydown = getKeyAndMove;
+nextButton.onclick = moveRight;
+prevButton.onclick = moveLeft;
 
 function getKeyAndMove(e){
 		var key_code=e.which||e.keyCode;
@@ -85,39 +121,38 @@ function getKeyAndMove(e){
 		}
 	}
 
+function moveRight() {
+    currentPicture += 1;
+    if (currentPicture > 3) {
+      currentPicture = 1;
+    }
+    move()
+  }
   function moveLeft() {
-    plusSlides(-1);
-  };
+    currentPicture -= 1;
+    if (currentPicture < 1) {
+      currentPicture = 3;
+    }
+    move()
+  }
 
-  function moveRight() {
-    plusSlides(1);
-  };
+function move(){
+  console.log("currentPicture", currentPicture);
+  carouselDiv.classList.remove("positionPic1");
+  carouselDiv.classList.remove("positionPic2");
+  carouselDiv.classList.remove("positionPic3");
+  pic1.classList.remove("fade");
+  pic2.classList.remove("fade");
+  pic3.classList.remove("fade");
 
-
-
-
-
-
-// <input type="text" onkeydown="myFunction()">
-//
-// <script>
-// function myFunction() {
-//   alert("You pressed a key inside the input field");
-// }
-
-
-// var xTriggered = 0;
-// $( "#target" ).keyup(function( event ) {
-//   xTriggered++;
-//   var msg = "Handler for .keyup() called " + xTriggered + " time(s).";
-//   $.print( msg, "html" );
-//   $.print( event );
-// }).keydown(function( event ) {
-//   if ( event.which == 13 ) {
-//     event.preventDefault();
-//   }
-// });
-//
-// $( "#other").click(function() {
-//   $( "#target" ).keyup();
-// });
+  if (currentPicture == 1) {
+      carouselDiv.classList.add("positionPic1");
+      pic1.classList.add("fade");
+  } else if (currentPicture == 2) {
+      carouselDiv.classList.add("positionPic2");
+      pic2.classList.add("fade");
+  } else if (currentPicture == 3) {
+      carouselDiv.classList.add("positionPic3");
+      pic3.classList.add("fade");
+  }
+}
