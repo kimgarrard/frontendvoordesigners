20b3f3b6dfe995c90b8db57267dbe7d4ec698489
Kim <kimgarrard030@gmail.com>
2019-05-28 15:30:36 +0200

    Versie 1 - basis + taal werkt

--- opdracht3/XMLHttpRequest/script.js
+++ opdracht3/XMLHttpRequest/script.js
@@ -3,62 +3,227 @@ console.log("huh?",this);
 //var uri = "https://open.data.amsterdam.nl/Attracties.json";
 //var uri = "https://open.data.amsterdam.nl/Activiteiten.json";
 //var uri = "https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json";
+// var uri = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
 
-var uri = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
+var uri = "https://open.data.amsterdam.nl/Attracties.json";
 
+var header = document.querySelector('header');
 var title = document.querySelector('h1');
 var section = document.querySelector('section');
 var button = document.querySelector("button");
 var loaderElement = document.querySelector(".loader");
 console.log("loader",loaderElement);
 
-////https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON
+var data; //wordt gevuld na het laden van de json
+var language = "nl";
+var eersteitem = 0;
+var laatsteitem = 30;
+
+
+
 function showData(jsonObj) {
-  var data = jsonObj;
+  data = jsonObj;
   console.log("JsonObj data",data);
-  console.log("squadName",data['squadName'])
 
-  //data uit de json halen
-  //var squadName = document.createElement('h2');
-  title.textContent = data['squadName'];
-  //var homeTown = document.createElement('p');
-  title.textContent += " - " + data['homeTown'];
-  title.textContent += " - " + data['secretBase'];
-  //header.appendChild(squadName);
-  //header.appendChild(homeTown);
+  // //data uit de json halen
+  // for (var i = 0; i < data.length; i++) {
 
-  var members = jsonObj['members'];
-  console.log("members",members);
+  for (var i = eersteitem; i < laatsteitem; i++) { //alleen de eerste 10 aanmaken ..
+    var attracties = document.createElement('article');
 
-  for (var i = 0; i < members.length; i++) {
-    var superhero = document.createElement('article');
+    var titel = document.createElement('h2');
+    titel.textContent = data[i].title;
 
-    var name = document.createElement('h2');
-    name.textContent = members[i].name;
+    var imgSection = document.createElement("section");
+    var media = data[i].media;
+    for (var a = 0; a < media.length; a++) {
+      // console.log("media", media);
+      var img = document.createElement("img");
+      img.src = media[a].url;
+      imgSection.appendChild(img);
 
-    var secretIdentity = document.createElement('p');
-    secretIdentity.textContent = "secret name: " + members[i].secretIdentity;
-
-    var age = document.createElement('span');
-    age.textContent = "leeftijd: " + members[i].age;
-
-    var powers = document.createElement('ul');
-    var superpowers = members[i].powers;
-    for(var j =0; j<superpowers.length; j++){
-      console.log("superpowers",superpowers[j])
-      var kracht = document.createElement('li');
-      kracht.textContent = superpowers[j];
-      powers.appendChild(kracht);
+      // if (img.src == "") {
+      //   img.classList.add("none");
+      // }
     }
 
-    superhero.appendChild(name);
-    superhero.appendChild(secretIdentity);
-    superhero.appendChild(age);
-    superhero.appendChild(powers);
 
-    section.appendChild(superhero);
-  }
+
+
+    var details = document.createElement("p");
+    var detaillong = data[i].details[language].longdescription;
+    var detailshort = data[i].details[language].shortdescription;
+      if (detaillong == "") {
+        detaillong = detailshort;
+      }
+    details.innerHTML = detaillong;
+
+    attracties.appendChild(titel);
+    attracties.appendChild(imgSection);
+
+    section.appendChild(attracties);
+    attracties.appendChild(details);
+
+
+  } //end: for loop data.length
+
+
+  console.log("functie aanroepen");
+  setupLanguage();
+
 } //end: function showdata
+
+
+
+
+
+function setupLanguage() {
+  console.log("setupLanguage");
+
+  var buttonDE = document.querySelector("nav button:nth-of-type(1)");
+  var buttonEN = document.querySelector("nav button:nth-of-type(2)");
+  var buttonFR = document.querySelector("nav button:nth-of-type(3)");
+  var buttonNL = document.querySelector("nav button:nth-of-type(4)");
+  var buttonIT = document.querySelector("nav button:nth-of-type(5)");
+  var buttonES = document.querySelector("nav button:nth-of-type(6)");
+
+
+  buttonDE.addEventListener("click", function(){
+    language = "de";
+    doedetaalwisseltruuk();
+  });
+
+  buttonEN.addEventListener("click", function(){
+    language = "en";
+    doedetaalwisseltruuk();
+  });
+
+  buttonFR.addEventListener("click", function(){
+    language = "fr";
+    doedetaalwisseltruuk();
+  });
+
+  buttonNL.addEventListener("click", function(){
+    language = "nl";
+    doedetaalwisseltruuk();
+  });
+
+  buttonIT.addEventListener("click", function(){
+    language = "it";
+    doedetaalwisseltruuk();
+  });
+
+  buttonES.addEventListener("click", function(){
+    language = "es";
+    doedetaalwisseltruuk();
+  });
+
+} //end: setupLanguage()
+
+
+function doedetaalwisseltruuk() {
+  console.log("doedetaalwisseltruuk 0", language, data[0]);
+    console.log("taal",data[0].details[language].shortdescription);
+
+    console.log("doedetaalwisseltruuk 1", language, data[1]);
+    console.log("taal",data[1].details[language].shortdescription);
+
+  //language = "es";
+
+  var details = document.querySelectorAll("article > p");
+
+    for (var p = 0; p < details.length; p++) {
+
+      var tekst = data[p].details[language].longdescription;
+        if (tekst == "") {
+          tekst =  data[p].details[language].shortdescription;
+        }
+      details[p].innerHTML = tekst;
+
+  }
+  // console.log("p",details)
+
+
+  // var i = 0;
+  // var detailtekst = data[i].details[language].longdescription;
+  // var detailtekst2 = data[i].details[language].shortdescription;
+  //   if (detailtekst == "") {
+  //     detailtekst = detailtekst2;
+  //   }
+  // details.innerHTML = detailtekst;
+
+
+} //end: doedetaalwisseltruuk()
+
+
+  // languageButton1.textContent = data[i].details.de.language;
+  // languageButton2.textContent = data.details.en.language;
+  // languageButton3.textContent = data.details.fr.language;
+  // languageButton4.textContent = data.details.nl.language;
+  // languageButton5.textContent = data.details.it.language;
+  // languageButton6.textContent = data.details.es.language;
+
+
+
+
+    //
+    // languageSelection.appendChild(languageButton1);
+    // languageSelection.appendChild(languageButton2);
+    // languageSelection.appendChild(languageButton3);
+    // languageSelection.appendChild(languageButton4);
+    // languageSelection.appendChild(languageButton5);
+    // languageSelection.appendChild(languageButton6);
+    //
+    // attracties.appendChild(details);
+    //
+    // section.appendChild(languageSelection);
+    //
+
+
+
+//
+//   var location1 = document.createElement("p");
+//   var locationtekst1 = data[i].location.name;
+//   var locationtekst2 = data[i].location.city;
+//   var locationtekst3 = data[i].location.adress;
+//   var locationtekst4 = data[i].location.zipcode;
+//   var locationtekst5 = data[i].location.latitude;
+//   var locationtekst6 = data[i].location.longitude;
+//
+//   location.textContent = locationtekst1;
+//   location.textContent = locationtekst2;
+//   location.textContent = locationtekst3;
+//   location.textContent = locationtekst4;
+//   location.textContent = locationtekst5;
+//   location.textContent = locationtekst6;
+//
+//
+//
+//   // var location = document.createElement('ul');
+//   // var locationdetails = data[i].location;
+//   // for(var l = 0; l < locationdetails.length; l++){
+//   //   var locdetails = document.createElement('li');
+//   //   locdetails.textContent = locationdetails[l];
+//   //   location.appendChild(locdetails);
+//   //   console.log("locdetails", locdetails);
+//   // }
+//
+//
+//
+//
+//
+//
+// // console.log("details", details);
+
+
+
+
+
+
+
+
+
+
 
 //https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
 //https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
@@ -93,9 +258,11 @@ function loadimagesmetXHR(){
 //loader feedback eerst onzichtbaar maken
 loaderElement.classList.add('hide');
 //actie
-button.onclick = function(){
+var laad = document.querySelector("main > button");
+// laad.onclick = function(){
+window.onload = function(){
   loaderElement.classList.remove('hide'); //loader wordt zichtbaar
-  this.classList.add('hide'); //button mag weg
+  // this.classList.add('hide'); //button mag weg
   section.innerHTML = ""; //main leeghalen. just in case
   loadimagesmetXHR();
 }; //end: button.onclick
