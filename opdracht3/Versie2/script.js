0d0f7de4804ff3b44ecbbd36d84590ac465041a4
Kim <kimgarrard030@gmail.com>
2019-06-08 15:47:11 +0200

    V2 - nav + slider

--- opdracht3/Versie2/script.js
+++ opdracht3/Versie2/script.js
@@ -19,8 +19,37 @@ console.log("loader",loaderElement);
 var data; //wordt gevuld na het laden van de json
 var language = "nl";
 var eersteitem = 0;
-var laatsteitem = 10;
+var laatsteitem = 30;
 
+var img1 = document.querySelector(".slider img:nth-of-type(1)");
+var img2 = document.querySelector(".slider img:nth-of-type(2)");
+var img3 = document.querySelector(".slider img:nth-of-type(3)");
+var img4 = document.querySelector(".slider img:nth-of-type(4)");
+
+// var tit2 = document.querySelector(".slidertekst");
+// var tit3 = document.querySelector(".slidertekst");
+// var tit4 = document.querySelector(".slidertekst");
+// var tit1 = document.querySelector(".slidertekst");
+
+
+var currentPicture = 1;
+var carouselDiv = document.querySelector(".slider div");
+
+
+
+
+var nextButton = document.querySelector(".next");
+var prevButton = document.querySelector(".prev");
+
+var slidertekst = document.querySelector(".slidertekst");
+// slidertekst.textContent = "hallo";
+
+// if (currentPicture == 1) {
+//   slidertekst.textContent = "hallo";
+// } else if (currentPicture == 2) {
+//   slidertekst.textContent = "doei";
+// }
+  // slidertekst.textContent = data[i].title[0];
 
 
 function showData(jsonObj) {
@@ -43,9 +72,21 @@ function showData(jsonObj) {
       var img = document.createElement("img");
       img.src = media[a].url;
       imgSection.appendChild(img);
+
+      // console.log("data[1].media[1]", data[1].media[1]);//
+
+      img1.src = data[26].media[0].url;
+      img2.src = data[28].media[0].url;
+      img3.src = data[15].media[2].url;
+      img4.src = data[23].media[0].url;
+
+
     }
 
-
+    //
+    // tit2.textContent = data[0].titel;
+    // tit3.textContent = data[1].titel;
+    // tit4.textContent = data[2].titel;
 
 
     var details = document.createElement("p");
@@ -57,10 +98,11 @@ function showData(jsonObj) {
     details.innerHTML = detaillong;
 
     attracties.appendChild(titel);
+    attracties.appendChild(details);
     attracties.appendChild(imgSection);
 
     section.appendChild(attracties);
-    attracties.appendChild(details);
+
 
 
   } //end: for loop data.length
@@ -68,11 +110,99 @@ function showData(jsonObj) {
 
   console.log("functie aanroepen");
   setupLanguage();
+  slider();
 
 } //end: function showdata
 
 
+function slider() {
+  nextButton.onclick = moveRight;
+  prevButton.onclick = moveLeft;
 
+  slidertekst.textContent = "Park Frankendael";
+
+
+    function moveRight() {
+
+      carouselDiv.classList.remove("positionPic1");
+      carouselDiv.classList.remove("positionPic2");
+      carouselDiv.classList.remove("positionPic3");
+      carouselDiv.classList.remove("positionPic4");
+      img1.classList.remove("fade");
+      img2.classList.remove("fade");
+      img3.classList.remove("fade");
+      img4.classList.remove("fade");
+
+
+      currentPicture += 1;
+        if (currentPicture > 4) {
+        currentPicture = 1;
+        }
+
+        if (currentPicture == 1) {
+          carouselDiv.classList.add("positionPic1");
+          img1.classList.add("fade");
+          slidertekst.textContent = "Park Frankendael";
+        } else if (currentPicture == 2) {
+          carouselDiv.classList.add("positionPic2");
+          img2.classList.add("fade");
+          slidertekst.textContent = "Gaasperplas Park";
+        } else if (currentPicture == 3) {
+          carouselDiv.classList.add("positionPic3");
+          img3.classList.add("fade");
+          slidertekst.textContent = "Gemeentemuseum Den Haag";
+        } else if (currentPicture == 4) {
+          carouselDiv.classList.add("positionPic4");
+          img4.classList.add("fade");
+          slidertekst.textContent = "De Oostvaardersplassen";
+        }
+    };
+
+    function moveLeft() {
+
+      carouselDiv.classList.remove("positionPic1");
+      carouselDiv.classList.remove("positionPic2");
+      carouselDiv.classList.remove("positionPic3");
+      carouselDiv.classList.remove("positionPic4");
+      img1.classList.remove("fade");
+      img2.classList.remove("fade");
+      img3.classList.remove("fade");
+      img4.classList.remove("fade");
+
+      currentPicture -= 1;
+        if (currentPicture < 1) {
+        currentPicture = 4;
+        }
+
+        if (currentPicture == 1) {
+          carouselDiv.classList.add("positionPic1");
+          img1.classList.add("fade");
+          slidertekst.textContent = "Park Frankendael";
+        } else if (currentPicture == 2) {
+          carouselDiv.classList.add("positionPic2");
+          img2.classList.add("fade");
+          slidertekst.textContent = "Gaasperplas Park";
+        } else if (currentPicture == 3) {
+          carouselDiv.classList.add("positionPic3");
+          img3.classList.add("fade");
+          slidertekst.textContent = "Gemeentemuseum Den Haag";
+        } else if (currentPicture == 4) {
+          carouselDiv.classList.add("positionPic4");
+          img4.classList.add("fade");
+          slidertekst.textContent = "De Oostvaardersplassen";
+        }
+    };
+
+    setInterval(moveRight, 5*1000);
+}
+
+// function test() {
+//   if (currentPicture == 1) {
+//     slidertekst.textContent = "hallo";
+//   } else if (currentPicture == 2) {
+//     slidertekst.textContent = "doei";
+//   }
+// }
 
 
 function setupLanguage() {
@@ -88,31 +218,85 @@ function setupLanguage() {
 
   buttonDE.addEventListener("click", function(){
     language = "de";
+
+    buttonDE.classList.remove("langSelected");
+    buttonEN.classList.remove("langSelected");
+    buttonFR.classList.remove("langSelected");
+    buttonNL.classList.remove("langSelected");
+    buttonIT.classList.remove("langSelected");
+    buttonES.classList.remove("langSelected");
+
+    buttonDE.classList.add("langSelected");
     doedetaalwisseltruuk();
   });
 
   buttonEN.addEventListener("click", function(){
     language = "en";
+
+    buttonDE.classList.remove("langSelected");
+    buttonEN.classList.remove("langSelected");
+    buttonFR.classList.remove("langSelected");
+    buttonNL.classList.remove("langSelected");
+    buttonIT.classList.remove("langSelected");
+    buttonES.classList.remove("langSelected");
+
+    buttonEN.classList.add("langSelected");
     doedetaalwisseltruuk();
   });
 
   buttonFR.addEventListener("click", function(){
     language = "fr";
+
+    buttonDE.classList.remove("langSelected");
+    buttonEN.classList.remove("langSelected");
+    buttonFR.classList.remove("langSelected");
+    buttonNL.classList.remove("langSelected");
+    buttonIT.classList.remove("langSelected");
+    buttonES.classList.remove("langSelected");
+
+    buttonFR.classList.add("langSelected");
     doedetaalwisseltruuk();
   });
 
   buttonNL.addEventListener("click", function(){
     language = "nl";
+
+    buttonDE.classList.remove("langSelected");
+    buttonEN.classList.remove("langSelected");
+    buttonFR.classList.remove("langSelected");
+    buttonNL.classList.remove("langSelected");
+    buttonIT.classList.remove("langSelected");
+    buttonES.classList.remove("langSelected");
+
+    buttonNL.classList.add("langSelected");
     doedetaalwisseltruuk();
   });
 
   buttonIT.addEventListener("click", function(){
     language = "it";
+
+    buttonDE.classList.remove("langSelected");
+    buttonEN.classList.remove("langSelected");
+    buttonFR.classList.remove("langSelected");
+    buttonNL.classList.remove("langSelected");
+    buttonIT.classList.remove("langSelected");
+    buttonES.classList.remove("langSelected");
+
+    buttonIT.classList.add("langSelected");
     doedetaalwisseltruuk();
   });
 
   buttonES.addEventListener("click", function(){
     language = "es";
+
+    buttonDE.classList.remove("langSelected");
+    buttonEN.classList.remove("langSelected");
+    buttonFR.classList.remove("langSelected");
+    buttonNL.classList.remove("langSelected");
+    buttonIT.classList.remove("langSelected");
+    buttonES.classList.remove("langSelected");
+
+    buttonES.classList.add("langSelected");
     doedetaalwisseltruuk();
   });
 
@@ -120,6 +304,7 @@ function setupLanguage() {
 
 
 function doedetaalwisseltruuk() {
+
   console.log("doedetaalwisseltruuk 0", language, data[0]);
     console.log("taal",data[0].details[language].shortdescription);
 
@@ -152,6 +337,8 @@ function doedetaalwisseltruuk() {
 
 
 } //end: doedetaalwisseltruuk()
+
+
 
 
   // languageButton1.textContent = data[i].details.de.language;
@@ -212,7 +399,6 @@ function doedetaalwisseltruuk() {
 //
 //
 // // console.log("details", details);
-
 
 
 
